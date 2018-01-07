f9f3528ee0719dcecc5e5dff6d821984908742ae
Dave Wongillies <dave.gillies@gmail.com>
2018-01-07 14:28:28 -0800

    Add available attributes for fpm/package/python

--- lib/fpm/cookery/package/python.rb
+++ lib/fpm/cookery/package/python.rb
@@ -12,8 +12,26 @@ module FPM
         def package_setup
           fpm.version = recipe.version
 
+          fpm.attributes[:python_bin?] = 'python'
+          fpm.attributes[:python_easyinstall?] = 'easy_install'
+          fpm.attributes[:python_pip?] = nil
+          fpm.attributes[:python_pypi?] = 'https://pypi.python.org/simple'
+
+          fpm.attributes[:python_package_name_prefix?] = 'python'
           fpm.attributes[:python_fix_name?] = true
           fpm.attributes[:python_fix_dependencies?] = true
+
+          fpm.attributes[:python_downcase_name?] = true
+          fpm.attributes[:python_downcase_dependencies?] = true
+
+          fpm.attributes[:python_install_bin?] = nil
+          fpm.attributes[:python_install_lib?] = nil
+          fpm.attributes[:python_install_data?] = nil
+          fpm.attributes[:python_dependencies?] = true
+          fpm.attributes[:python_obey_requirements_txt?] = nil
+          fpm.attributes[:python_scripts_executable?] = nil
+          fpm.attributes[:python_disable_dependency?] = []
+
         end
 
         def package_input
